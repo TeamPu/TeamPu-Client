dcdbdc4564d8dbb01fe40295dc1dd0201199cf84
cho4u4o <cho4u4o@gmail.com>
2024-11-21 17:54:35 +0900

    design : adjust layout

--- src/ReservationStatusSection.jsx
+++ src/ReservationStatusSection.jsx
@@ -2,13 +2,11 @@ import HomeCalendar from "./HomeCalendar";
 
 export default function ReservationStatusSection() {
   return (
-    <div className="card h-full">
+    <div className="card flex h-full flex-col justify-between">
       <p className="title">
         <span className="text-primary">11월</span> 야간 잔류 신청 현황
       </p>
-      <div className="grid h-full place-items-center">
-        <HomeCalendar showDoubleView={false} />
-      </div>
+      <HomeCalendar showDoubleView={false} />
     </div>
   );
 }
