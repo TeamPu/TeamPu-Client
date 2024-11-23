ccd4e3c605c0051acb696a8c454decdaf4eca637
cho4u4o <cho4u4o@gmail.com>
2024-11-24 02:05:33 +0900

    feat : make pages and navbar

--- src/containers/SideNavBar.jsx
+++ src/containers/SideNavBar.jsx
@@ -1,11 +1,24 @@
+import { Link } from "react-router-dom";
 import Logo from "../assets/TeamPuLogo.png";
 export default function SideNavBar() {
+  const links = ["/", "/reservation", "/mypage", "rules"];
+  const menus = ["홈", "내 예약", "내 정보 관리", "이용수칙"];
   return (
-    <div className="grid h-full w-[220px] bg-white px-5 py-14">
-      <div className="grid h-fit w-full place-items-center">
-        <img src={Logo} className="w-[150px]" />
+    <div className="h-full w-[300px] bg-white px-10 py-14">
+      <div>
+        <div className="mb-20 h-fit w-full">
+          <img src={Logo} />
+        </div>
+        <div className="flex flex-col gap-5">
+          {links.map((link, i) => {
+            return (
+              <Link key={i} to={link} className="menu">
+                {menus[i]}
+              </Link>
+            );
+          })}
+        </div>
       </div>
-      <div></div>
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as HomePage } from "./HomePage";
+export { default as MyReservationPage } from "./MyReservationPage";
+export { default as MyPage } from "./MyPage";
+export { default as RulesPage } from "./RulesPage";
