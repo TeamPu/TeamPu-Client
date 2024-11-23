adf7b7dd745ce7563456415863bd54c4d91eb5b4
cho4u4o <cho4u4o@gmail.com>
2024-11-24 02:06:27 +0900

    feat : separate shared styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import ReservationStatusSection from "../containers/ReservationStatusSection";
 
 export default function HomePage() {
   return (
-    <section className="box-border grid h-full w-full place-items-center gap-5 overflow-hidden p-[20px]">
+    <section className="section">
       <ReservationStatusSection />
     </section>
   );
