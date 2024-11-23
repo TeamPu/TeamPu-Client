415b4bb937315ea6456bbb6b2f0d12fe0f9a5085
cho4u4o <cho4u4o@gmail.com>
2024-11-24 02:06:00 +0900

    feat : set pages router

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomePage, MyReservationPage, MyPage, RulesPage } from "./pages";
 import AppFrame from "./layouts/AppFrame.jsx";
-import HomePage from "./pages/HomePage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <AppFrame />,
-    children: [{ index: true, element: <HomePage /> }],
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "/reservation", element: <MyReservationPage /> },
+      { path: "/mypage", element: <MyPage /> },
+      { path: "/rules", element: <RulesPage /> },
+    ],
   },
 ]);
 
