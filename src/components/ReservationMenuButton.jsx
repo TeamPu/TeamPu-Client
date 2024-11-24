5b321fd357c12bcfaedd986cc8c7a73c6e8420c5
cho4u4o <cho4u4o@gmail.com>
2024-11-25 02:53:24 +0900

    feat : make reservation menu button

--- src/components/ReservationMenuButton.jsx
+++ src/components/ReservationMenuButton.jsx
@@ -1,7 +1,4 @@
-import { useReservationMenu } from "../hooks/useReservationMenu";
-
-export default function ReservationMenuButton() {
-  const { selected, handleClicked } = useReservationMenu();
+export default function ReservationMenuButton({ selected, handleClicked }) {
   const menus = ["승인 완료", "대기중", "지난 예약", "취소된 예약"];
 
   return (
@@ -18,39 +15,3 @@ export default function ReservationMenuButton() {
     </div>
   );
 }
-// const MenuReviewToggleButton = styled.div`
-//   width: 86px;
-//   height: 30px;
-//   background-color: #333;
-//   margin-left: 10px;
-//   border-radius: 4px;
-//   align-items: center;
-//   justify-content: space-between;
-//   display: flex;
-//   padding: 2px;
-//   box-sizing: border-box;
-// `;
-
-// const MenuReviewToggleWrapper = styled.div`
-//   display: flex;
-//   height: 30px;
-//   width: 100%;
-//   margin-bottom: 10px;
-
-//   @media (max-width: 480px) {
-//     margin-bottom: 0px;
-//   }
-// `;
-// const MenuReviewToggleContent = styled.button`
-//   width: 40px;
-//   height: 26px;
-//   background-color: ${({ isSelected }) => (isSelected ? '#444' : '#333')};
-//   color: ${({ isSelected }) => (isSelected ? '#fff' : '#aaa')};
-//   font: 500 12px/1.5 Pretendard;
-//   border: none;
-//   border-radius: 2px;
-
-//   @media (max-width: 480px) {
-//     font-size: 10px;
-//   }
-// `;
