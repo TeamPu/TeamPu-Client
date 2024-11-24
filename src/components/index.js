8a11f2ae11d2845eb56589079104b4765ab39c31
cho4u4o <cho4u4o@gmail.com>
2024-11-25 02:53:44 +0900

    feat : make my reservation page

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as CalendarNavButton } from "./CalendarNavButton";
 export { default as HomeCalendar } from "./HomeCalendar";
 export { default as ReservationMenuButton } from "./ReservationMenuButton";
+export { default as ReservationView } from "./ReservationView";
--- src/pages/MyReservationPage.jsx
+++ src/pages/MyReservationPage.jsx
@@ -1,11 +1,16 @@
-import { ReservationMenuButton } from "../components/";
+import { ReservationMenuButton, ReservationView } from "../components/";
+import { useReservationMenu } from "../hooks/";
 
 export default function MyReservationPage() {
+  const { selected, handleClicked } = useReservationMenu();
+  const menuProps = { selected, handleClicked };
+
   return (
     <div className="section">
-      <div className="card h-[70vh] w-[55vw]">
+      <div className="card cardSize flex flex-col">
         <p className="title">내 예약</p>
-        <ReservationMenuButton />
+        <ReservationMenuButton {...menuProps} />
+        <ReservationView {...menuProps} />
       </div>
     </div>
   );
