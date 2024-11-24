4a6234418372e16a316555bd9015e6867c08cb0e
cho4u4o <cho4u4o@gmail.com>
2024-11-25 03:08:40 +0900

    feat : make shared border style

--- src/components/MyPageForm.jsx
+++ src/components/MyPageForm.jsx
@@ -1,3 +1,10 @@
 export default function MyPageForm() {
-  return <div className="mt-5 flex flex-col gap-y-2.5">MyPageForm</div>;
+  return (
+    <div className="mt-5 flex flex-col gap-y-2.5">
+      <div className="flex flex-col gap-y-2.5">
+        <label htmlFor="name">이름</label>
+        <input type="text" id="name" name="name" className="shared-border" />
+      </div>
+    </div>
+  );
 }
