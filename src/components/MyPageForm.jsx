411553cbc05f964b2e3a18f1644ae33dfcad3aff
cho4u4o <cho4u4o@gmail.com>
2024-11-25 03:03:49 +0900

    feat : make mypageform component

---
+++ src/components/MyPageForm.jsx
@@ -0,0 +1,3 @@
+export default function MyPageForm() {
+  return <div className="mt-5 flex flex-col gap-y-2.5">MyPageForm</div>;
+}
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,3 +1,12 @@
+import { MyPageForm } from "../components";
+
 export default function MyPage() {
-  return <div className="section">UserInfoPage</div>;
+  return (
+    <div className="section">
+      <div className="card cardSize flex flex-col">
+        <p className="title">내 정보 관리</p>
+        <MyPageForm />
+      </div>
+    </div>
+  );
 }
