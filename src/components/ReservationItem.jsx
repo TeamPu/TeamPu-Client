68b924943426a35d20210caeac70701de6ccbc66
cho4u4o <cho4u4o@gmail.com>
2024-11-28 17:41:31 +0900

    feat : add button on reservation item

--- src/components/ReservationItem.jsx
+++ src/components/ReservationItem.jsx
@@ -1,13 +1,28 @@
-export default function ReservationItem({ year, date, time, people }) {
+export default function ReservationItem({ year, date, time, people, admin }) {
   return (
-    <div className="shared-border flex h-20 w-full gap-x-7 bg-secondary-light px-4 py-6">
-      <div className="flex h-full flex-col justify-center">
-        <p className="text-center text-sm font-semibold">{year}</p>
-        <p className="text-center text-xl font-bold text-primary">{date}</p>
+    <div className="shared-border flex h-20 w-full justify-between bg-secondary-light px-4 py-6">
+      <div className="flex gap-x-7">
+        <div className="flex h-full flex-col justify-center">
+          <p className="text-center text-sm font-semibold">{year}</p>
+          <p className="text-center text-xl font-bold text-primary">{date}</p>
+        </div>
+        <div className="flex h-full flex-col justify-center">
+          <p className="text-center text-sm">{time}</p>
+          <p className="text-center text-xl font-semibold">{people}</p>
+        </div>
       </div>
-      <div className="flex h-full flex-col justify-center">
-        <p className="text-center text-sm">{time}</p>
-        <p className="text-center text-xl font-semibold">{people}</p>
+      <div className="flex w-20 flex-col justify-center gap-y-1">
+        {admin ? (
+          <>
+            <button className="button res-positive-button">승인</button>
+            <button className="res-negative-button">거부</button>
+          </>
+        ) : (
+          <>
+            <button className="button res-positive-button">편집</button>
+            <button className="res-negative-button">취소</button>
+          </>
+        )}
       </div>
     </div>
   );
