886e03f80234c91e930f81f0b274048f269a1bec
cho4u4o <cho4u4o@gmail.com>
2024-12-02 02:23:21 +0900

    design : add home logo in mobile version

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -32,6 +32,16 @@ export default function HomePage() {
   return (
     <section className="section">
       <div className="card">
+        <div className="visible mb-10 flex flex-col items-center md:hidden">
+          <p className="epilogue h-fit w-full text-center text-5xl font-extrabold">
+            <span className="text-primary">Team</span>Pu
+            <br />
+          </p>
+          <p className="m-0 text-center text-sm text-black">
+            팀프로젝트실 야간 잔류 신청 시스템
+          </p>
+        </div>
+
         <div className="flex justify-between">
           <div className="flex gap-x-4">
             <p className="title">야간 잔류 신청 현황</p>
