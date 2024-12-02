673045d4a24ee7280b12c6e06ff9bef777a6dbc1
cho4u4o <cho4u4o@gmail.com>
2024-12-02 13:07:56 +0900

    fix : fix button width

--- src/components/ReservationMenuButton.jsx
+++ src/components/ReservationMenuButton.jsx
@@ -2,7 +2,7 @@ export default function ReservationMenuButton({ selected, handleClicked }) {
   const menus = ["승인 완료", "대기중", "지난 예약", "취소된 예약"];
 
   return (
-    <div className="my-5 flex h-10 rounded-lg bg-white p-1 md:w-fit md:bg-secondary-light">
+    <div className="my-5 flex h-10 w-fit rounded-lg bg-white p-1 md:bg-secondary-light">
       {menus.map((menu, index) => (
         <div
           key={index}
