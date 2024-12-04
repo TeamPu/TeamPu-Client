43ed4e12a9deee4f706da7cabcbf3d2c461404ae
cho4u4o <cho4u4o@gmail.com>
2024-12-04 17:10:46 +0900

    feat : activate conditional rendering

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,11 +1,29 @@
+import { Link } from "react-router-dom";
 import { MyPageForm } from "../components";
+import { getCookie } from "../utils";
 
 export default function MyPage() {
   return (
     <div className="section">
       <div className="card cardSize flex flex-col">
-        <p className="title">내 정보 관리</p>
-        <MyPageForm />
+        {getCookie("token") ? (
+          <>
+            <p className="title">내 정보 관리</p>
+            <MyPageForm />
+          </>
+        ) : (
+          <div className="grid h-full w-full place-items-center">
+            <div className="flex flex-col items-center justify-center gap-y-2.5">
+              로그인 후 이용 가능해요!
+              <Link
+                to="/login"
+                className="shared-border px-4 py-2 transition duration-300 ease-in-out hover:bg-primary hover:text-white"
+              >
+                로그인하기
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/pages/MyReservationPage.jsx
+++ src/pages/MyReservationPage.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
 import { ReservationMenuButton, ReservationView } from "../components/";
 import { useReservationMenu } from "../hooks/";
+import { getCookie } from "../utils";
 
 export default function MyReservationPage() {
   const { selected, handleClicked } = useReservationMenu();
@@ -8,9 +10,25 @@ export default function MyReservationPage() {
   return (
     <div className="section">
       <div className="card cardSize flex flex-col">
-        <p className="title">내 예약</p>
-        <ReservationMenuButton {...menuProps} />
-        <ReservationView {...menuProps} />
+        {getCookie("token") ? (
+          <>
+            <p className="title">내 예약</p>
+            <ReservationMenuButton {...menuProps} />
+            <ReservationView {...menuProps} />
+          </>
+        ) : (
+          <div className="grid h-full w-full place-items-center">
+            <div className="flex flex-col items-center justify-center gap-y-2.5">
+              로그인 후 이용 가능해요!
+              <Link
+                to="/login"
+                className="shared-border px-4 py-2 transition duration-300 ease-in-out hover:bg-primary hover:text-white"
+              >
+                로그인하기
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
